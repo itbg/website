88c99470c1c2d4afdb29606bc4d688d6df91cb0b
Vladislav Mihov <vlad.slav.vo.wo@gmail.com>
2021-10-28 19:15:27 +0300

    add rules.

---
+++ rules/script.js
@@ -0,0 +1,15 @@
+var div = document.createElement('div');
+// div.textContent = 'I \u2764\uFE0F emoji!';
+document.body.appendChild(div);
+
+twemoji.parse(document.body);
+
+var img = div.querySelector('img');
+
+// note the div is preserved
+img.parentNode === div; // true
+
+img.src;        // https://twemoji.maxcdn.com/v/latest/72x72/2764.png
+img.alt;        // \u2764\uFE0F
+img.className;  // emoji
+img.draggable;  // false
